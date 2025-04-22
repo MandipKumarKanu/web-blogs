8f453e1e62f37a30395e7626df31ad0cd0b3e135
mandipkumarkanu <mandyshah2003@gmail.com>
2025-04-22 09:06:56 +0545

    progressive web app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,3 +9,17 @@ createRoot(document.getElementById("root")).render(
       <App />
     </BrowserRouter>
 );
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(
+      registration => {
+        console.log('ServiceWorker registration successful:', registration);
+      },
+      err => {
+        console.error('ServiceWorker registration failed:', err);
+      }
+    );
+  });
+}
